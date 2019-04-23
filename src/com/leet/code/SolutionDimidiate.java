5364e7ae112e2848e31b74cb361d80a29b0a01f1
yuqiu-pp <41912446@qq.com>
2019-04-23 22:33:47 +0800

    update

--- src/com/leet/code/SolutionDimidiate.java
+++ src/com/leet/code/SolutionDimidiate.java
@@ -2,12 +2,68 @@ package com.leet.code;
 
 public class SolutionDimidiate {
 
+
+
+
+
+    /**
+     *  执行用时 : 2 ms, 在Pow(x, n)的Java提交中击败了99.78% 的用户
+     内存消耗 : 32.9 MB, 在Pow(x, n)的Java提交中击败了89.11% 的用户
+     */
+    // 把幂次方n除2，结果就是两个幂次方是n/2的数的相乘，这样只需要计算1次n/2次方的运算
+    // 依次类推，直到指数变为1，不能再分
+    // 如果指数为奇数，二分之后，计算结果时需要多乘一次x
+    // LeetCode提交，报栈溢出
     public double myPow(double x, int n) {
-        double p = 0;
-        double q = 0;
+        // 递归出口
+        if (n == 0){
+            return 1;
+        }
+        if (n == 1){
+            return x;
+        }
+        // 指数是负数
+        if (n == -1){
+            return 1/x;
+        }
 
-        // n 偶数  分解成
+        // 计算n/2次方
+        double rst = myPow(x, n/2);
+        // 计算n次方
+        rst = rst * rst;
+        // 奇次方需要多乘一次
+        if (n % 2 != 0){
+            if (n > 0){
+                rst *= x;
+            }else {
+                rst *= 1/x;
+            }
 
-        return p;
+        }
+
+        return rst;
+    }
+
+
+
+    /**
+     *  执行用时 : 22 ms, 在Arranging Coins的Java提交中击败了79.62% 的用户
+     内存消耗 : 32.8 MB, 在Arranging Coins的Java提交中击败了92.65% 的用户
+     */
+    public int arrangeCoins(int n) {
+        if (n == 0){
+            return 0;
+        }
+
+        // 循环分解，直到剩余数量 < 分解次数+1
+        int i = 0;
+        for (i = 1; i < n; i++) {
+            n -= i;
+            if (n < i + 1){
+                break;
+            }
+        }
+
+        return i;
     }
 }
--- src/com/leet/code/SolutionSort.java
+++ src/com/leet/code/SolutionSort.java
@@ -4,21 +4,42 @@ import java.util.Arrays;
 
 public class SolutionSort {
 
+    // LeetCode直接运行是奇数、偶数个元素得到结果
+    // 偶数个元素                 奇数个元素
+    // [1, 6, 1, 5, 1, 4]        [2, 6, 1, 5, 1]
+    //  小 大 小 大  小 大         小 大 小 大 小
+    // 要符合上面的格式，需要对素组进行排序，然后分成大、小两部分
+    // 元素是奇数个时，小的那部分要多一个元素
     public void wiggleSort(int[] nums) {
-        // LeetCode直接运行是奇数、偶数个元素得到结果
-        // [2, 3, 1, 3, 2]   [2, 3, 1, 3, 1, 2]
-        //  小 大 小  大 小     小 大 小  大 小 大
-        // 要实现上面的顺序，需要将数组排序，
-
-        // 排序
-        Arrays.sort(nums);
-
-        // 分两半，每次从前半取一个数，后半取一个数
         int len = nums.length;
 
+        if (len == 0){
+            return;
+        }
+        // 数组排序
+        int[] rst = Arrays.copyOf(nums, len);
+        Arrays.sort(rst);
+        for (int i = 0; i < len; i++) {
+            nums[i] = 0;
+        }
 
-
+        // 取大数部分的起始位置
+        int big = 0;
+        if (len % 2 == 0){
+            big = len / 2;
+        }else {
+            big = len / 2 + 1;
+            nums[len-1] = rst[0];
+        }
+        // 每次分别从大、小两部分取一个数写入
+        // [4,5,5,6] 分别从两部分的前面开始取数，不能满足这种情况，所以从后往前取数
+        for (int i = 0; i < len - big; i++) {
+            nums[i*2] = rst[big - 1 - i];
+            nums[i*2+1] = rst[len -1 - i];
+        }
     }
+
+
 
 
     public static void main(String[] args) {
