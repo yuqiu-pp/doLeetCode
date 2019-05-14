e51d4d7cdd2050f55763e21de6bd7436f081ed49
yuqiu-pp <41912446@qq.com>
2019-05-14 22:00:05 +0800

    update

--- src/com/leet/code/SolutionBST.java
+++ src/com/leet/code/SolutionBST.java
@@ -1,22 +1,95 @@
 package com.leet.code;
 
 
+import com.sun.org.apache.xerces.internal.impl.dv.dtd.NMTOKENDatatypeValidator;
+
+import java.math.BigDecimal;
+import java.util.Stack;
+
 // 二叉搜索树
 // 左节点小于父节点，右节点大于父节点
 // 查找效率高
 public class SolutionBST {
 
+    // LeetCode 938
+
+    // 遍历，把值在 [L,R] 之间的所有节点找出来
+    // public int rangeSumBST(TreeNode root, int L, int R) {
+    //     if (root == null){
+    //         return 0;
+    //     }
+    //
+    //     if (root.val >= L && root.val <= R){
+    //         stack.push(root.val);
+    //     }
+    //     if (root.val <= L){
+    //         findValInRegion(root.right, L, R, stack);
+    //     }else if (root.val >= R){
+    //         findValInRegion(root.left, L, R, stack);
+    //     }else {
+    //         findValInRegion(root.left, L, R, stack);
+    //         findValInRegion(root.right, L, R, stack);
+    //     }
+    //
+    //
+    //     return sum;
+    // }
+
+    /**
+     *  执行用时 : 8 ms, 在Range Sum of BST的Java提交中击败了25.00% 的用户
+        内存消耗 : 52.4 MB, 在Range Sum of BST的Java提交中击败了43.90% 的用户
+     */
     // 遍历，把值在 [L,R] 之间的所有节点找出来
     public int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null){
+            return 0;
+        }
 
-        if ()
+        Stack<Integer> stack = new Stack<>();
+        findValInRegion(root, L, R, stack);
 
-        return 0;
+        int sum = 0;
+        while (!stack.isEmpty()){
+            sum += stack.pop();
+        }
+        return sum;
     }
 
-    public void traverseBST(TreeNode root, int L, int R){
+    /**
+     *  执行用时 : 7 ms, 在Range Sum of BST的Java提交中击败了31.48% 的用户
+        内存消耗 : 52.8 MB, 在Range Sum of BST的Java提交中击败了35.37% 的用户
+     */
+    // 属于区间的存入栈，同时利用搜索树的特点
+    public void findValInRegion(TreeNode root, int L, int R, Stack<Integer> stack){
+        if (root == null){
+            return;
+        }
 
-        if (root.val >)
+        if (root.val >= L && root.val <= R){
+            stack.push(root.val);
+        }
+        if (root.val <= L){
+            findValInRegion(root.right, L, R, stack);
+        }else if (root.val >= R){
+            findValInRegion(root.left, L, R, stack);
+        }else {
+            findValInRegion(root.left, L, R, stack);
+            findValInRegion(root.right, L, R, stack);
+        }
+    }
+
+    // 属于区间的存入栈
+    public void findValInRegion1(TreeNode root, int L, int R, Stack<Integer> stack){
+        if (root == null){
+            return;
+        }
+        // left小于
+        if (root.val >= L && root.val <= R){
+            stack.push(root.val);
+        }
+
+        findValInRegion1(root.left, L, R, stack);
+        findValInRegion1(root.right, L, R, stack);
     }
 
 
@@ -39,14 +112,116 @@ public class SolutionBST {
         return node;
     }
 
+
+    // LeetCode 220
+
+    // 每个元素和后面的元素计算差
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i+1; j < len; j++) {
+                int m = j - i;
+                int p = Math.abs(nums[i]);
+                int q = Math.abs(nums[j]);
+                if ((nums[i]>=0 && nums[j]<0) || (nums[i]<0 && nums[j]>=0)){
+                    if (p > m || q > m){
+                        return false;
+                    }
+                }
+                if (Math.abs(p-q) <=t && (m <= k)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean containsNearbyAlmostDuplicate1(int[] nums, int k, int t) {
+
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i+1; j < len; j++) {
+                BigDecimal m = new BigDecimal(nums[i]);
+                BigDecimal n = new BigDecimal(nums[j]);
+                m = m.subtract(n).abs();
+                if ((m.compareTo(new BigDecimal(t)) == -1) && ((j - i) <= k)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+
+    // LeetCode 783
+
+
+    /**
+     *  执行用时 : 14 ms, 在Minimum Distance Between BST Nodes的Java提交中击败了5.45% 的用户
+        内存消耗 : 34.6 MB, 在Minimum Distance Between BST Nodes的Java提交中击败了74.41% 的用户
+     */
+    // 中序遍历得到一个，然后计算两两差值
+    public int minDiffInBST(TreeNode root) {
+
+        Stack<Integer> stack = new Stack<>();
+        traverseBSF(root, stack);
+
+        int[] array = new int[stack.size()];
+        for (int i = 0; !stack.isEmpty(); i++) {
+            array[i] = stack.pop();
+        }
+
+        int len = array.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                int m = array[i] - array[j];
+                if (stack.isEmpty()){
+                    stack.push(m);
+                }else {
+                    if (m < stack.peek()){
+                        stack.pop();
+                        stack.push(m);
+                    }
+                }
+            }
+        }
+
+        return stack.pop();
+    }
+
+    // 中序遍历，有序输出
+    public void traverseBSF(TreeNode root, Stack<Integer> stack){
+        if (root == null){
+            return;
+        }
+
+        traverseBSF(root.left, stack);
+        stack.push(root.val);
+        traverseBSF(root.right, stack);
+    }
+
+
     public static void main(String[] args) {
         SolutionBST solution = new SolutionBST();
 
-        Integer[] B = {10,5,15,3,7,null,18};
+        Integer[] B = {10,5,15,3,7,13,18,1,null,6};
 
-        TreeNode root = solution.createBSTByArray(B, 0);
+        // TreeNode root = solution.createBSTByArray(B, 0);
+        //
+        // int n = solution.rangeSumBST(root, 6, 10);
+        // System.out.println(n);
 
-        int n = solution.rangeSumBST(root, 7, 15);
-        System.out.println(n);
+        // int[] A = {2, 0,-2, 2};
+        // System.out.println(solution.containsNearbyAlmostDuplicate1(A, 1, 2147483647));
+
+
+        Integer[] C = {4,2,6,1,3,null,null};
+        TreeNode root = solution.createBSTByArray(C, 0);
+        System.out.println(solution.minDiffInBST(root));
+
     }
 }
--- src/com/leet/code/SolutionGreedy.java
+++ src/com/leet/code/SolutionGreedy.java
@@ -26,7 +26,6 @@ public class SolutionGreedy {
                     num ++;
                     break;
                 }
-
             }
             if (curr > s.length){
                 break;
@@ -37,13 +36,62 @@ public class SolutionGreedy {
     }
 
 
+
+    // LeetCode 714
+
+    /**
+     *  执行用时 : 9 ms, 在Best Time to Buy and Sell Stock with Transaction Fee的Java提交中击败了94.95% 的用户
+        内存消耗 : 72 MB, 在Best Time to Buy and Sell Stock with Transaction Fee的Java提交中击败了23.64% 的用户
+     */
+
+    // 贪心算法求解
+    // 1）问题抽象：n天股票价格中，抽取某些天进行买卖
+    // 2）利润最大：最低价买，最高价卖
+    // 手续费意味着：如果多卖一次的利润 <= fee，那就应该少卖一次
+    // 高点卖出后，如果股票下跌超过fee，那么卖出就是正确的；
+    // 下跌 prices[i] - prices[i+1] > fee，即 prices[i]-fee > prices[i+1]
+    // 所以卖出后，设置minPrice = prices[i] - fee
+
+    // 当卖亏了怎么办？
+    // 把价格拉低补回来
+
+    public int maxProfit(int[] prices, int fee) {
+        if (prices.length <= 1){
+            return 0;
+        }
+
+        int minPirce = prices[0];
+        int sum = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] <= minPirce){
+                minPirce = prices[i];
+            }else {
+                // 当天价格卖出利润大于0
+                int n = prices[i] - minPirce - fee;
+                if (n > 0){
+                    sum += n;
+                    // 后面有可能存在利润更大的，如果出现这种情况，把那部分利润补回来就行了
+                    // ？？
+                    minPirce = prices[i] - fee;
+                }
+            }
+        }
+        return sum;
+    }
+
+
+
     public static void main(String[] args) {
         SolutionGreedy solution = new SolutionGreedy();
 
-        int[] children = {250,490,328,149,495,325,314,360,333,418,430,458};
-        int[] s = {29,310,236,441,200,267,115,59,277,42,361,112,483,104,338,69,438,55,318,470,20,490,455,119,259,51,492,50,160,414,38,289,429,446,350,412,12,515,367,397,122,35,522,355,448,266,333,500,211,226,203,366,240,324,111,280,520,321,211,360,437,292,512,161,85,139,12,211,236,213,377,85,494};
-        System.out.println(s.length);
-        System.out.println(solution.findContentChildren(children, s));
+        // int[] children = {250,490,328,149,495,325,314,360,333,418,430,458};
+        // int[] s = {29,310,236,441,200,267,115,59,277,42,361,112,483,104,338,69,438,55,318,470,20,490,455,119,259,51,492,50,160,414,38,289,429,446,350,412,12,515,367,397,122,35,522,355,448,266,333,500,211,226,203,366,240,324,111,280,520,321,211,360,437,292,512,161,85,139,12,211,236,213,377,85,494};
+        // System.out.println(s.length);
+        // System.out.println(solution.findContentChildren(children, s));
+
+        int[] prices = {1,3,7,5,10,3};
+        System.out.println(solution.maxProfit(prices, 3));
     }
 
 }
